48368c5a1a8382dc5ae749ba83a20f73a2d60aa7
PhantomOblivion <smasif404@gmail.com>
2026-02-06 15:58:23 +0600

    Solved the problems part1 task-1.

--- task-1.js
+++ task-1.js
@@ -1,1 +1,8 @@
 // Task-1: Write a function to convert temperature from Celsius to Fahrenheit.
+
+function temperature (celsius){
+    let fahrenheit = (celsius * 9/5) + 32; //formula to convert the temperature from celsius to fahrenheit.
+    return fahrenheit;
+}
+
+console.log (temperature(37));
